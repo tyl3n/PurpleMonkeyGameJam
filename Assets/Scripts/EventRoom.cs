203804ef92c49ad3e2597d5a5876406f53dd614c
ZED <corbettz@bu.edu>
2017-04-22 14:37:06 -0400

    Adding EventRoom

---
+++ Assets/Scripts/EventRoom.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Collider))]
+public class EventRoom : MonoBehaviour {
+
+	private GameManager gm;
+	private float fixRate = 1f;
+	private float fragRate = 0.1f;
+
+	public float perileVal = 0;
+	public bool inPerile = false;
+
+	// Use this for initialization
+	void Start () {
+		gm = GameManager.instance;
+		this.gameObject.GetComponent<Collider>().isTrigger = true;
+	}
+
+	public void OnTriggerEnter(Collider c){
+		// if character enters room
+			// if room inPerile
+				// if character doing "fixing" action
+					// lower perileVal by n (fixRate)
+			// else
+				// do nothing
+	}
+	
+
+}
