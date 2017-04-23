9d2fa6fcf2e72c0fabd7b1bdd4a48e02626d8bbe
Antoine McNabb-Baltar <antoine.mc-nabb-baltar.1@ens.etsmtl.ca>
2017-04-23 14:08:16 -0400

    Animation

---
+++ Assets/Scripts/FootStepController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FootStepController : MonoBehaviour {
+    SpriteRenderer spriteRender;
+    // Use this for initialization
+    void Start () {
+        spriteRender = GetComponent<SpriteRenderer>();
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
