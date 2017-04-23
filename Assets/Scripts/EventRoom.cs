c8b52ed39c5583fddb6e5b7119eaa149ae73ec0c
ZED <corbettz@bu.edu>
2017-04-22 23:38:54 -0400

    Room break and heal timers now work.

--- Assets/Scripts/EventRoom.cs
+++ Assets/Scripts/EventRoom.cs
@@ -5,8 +5,10 @@ using System.Collections;
 public class EventRoom : MonoBehaviour {
 
 	public float timeUntilPeril = 10f; // in seconds
-	public float perilDecrRate = 1f;
-	public float perilIncrRate = 0.1f;
+	public float roomHealAmount = -0.1f;
+	public float roomHealSpeed = 0.5f; // in seconds
+	public float roomBreakAmount = 0.1f;
+	public float roomBreakSpeed = 0.5f; // in seconds
 
 
 	private string PLAYER_TAG = "Player";
@@ -14,9 +16,12 @@ public class EventRoom : MonoBehaviour {
 
 	private float perilCountDownTimer;
 
-	private float perilVal;
+	private float perilValue;
 	private const float MAX_PERIL = 1f;
 	private const float MIN_PERIL = 0f;
+
+	private float perilChangeRate = 1f;
+	private float perilChangeAmount = 0;
 
 	public bool inPeril = false; // public for debugging
 	public bool plyrInRoom = false; // public for debugging
@@ -29,28 +34,35 @@ public class EventRoom : MonoBehaviour {
 		// Make sure object's trigger is on
 		this.gameObject.GetComponent<Collider>().isTrigger = true;
 
-		perilVal = MIN_PERIL;
+		perilValue = MIN_PERIL;
 
 		this.ResetPerilTrigger();
 		StartCoroutine(ManagePerilTrigger());
+		StartCoroutine(ChangePerilValue());
 	}
 
 	void FixedUpdate(){
-		if(inPeril){ Debug.Log ("perilValue: " + perilVal); }
+		if(inPeril){ Debug.Log ("perilValue: " + perilValue); }
 
 		if(plyrInRoom) {
 			/***DECREASE PERIL***/
 			// When room is in perile and character doing "fixing" action
-			if(inPeril && Input.GetKeyDown (KeyCode.Space)) {
-				// Lower perile value by fix rate
-				Invoke("decrPerilValue",1);
+			if(inPeril){
+				this.DontChangePerilValue();
+				if (Input.GetKeyUp(KeyCode.Space)) {
+					// Lower perile value by fix rate
+//					this.DecrPerilValue();
+					this.ManuallyDecrPerilValue();
+
+				} else if(Input.GetKey(KeyCode.Space)){
+					this.DecrPerilValue();
+				}
 			}
 		} else {
 
 			/***INCREASE PERIL***/
 			if(inPeril) {
-				incrPerilValue();
-				//				Invoke("incrPerilValue",1);
+				this.IncrPerilValue();
 			}
 		}
 	}
@@ -72,49 +84,74 @@ public class EventRoom : MonoBehaviour {
 		}
 	}
 
-	public float GetPerilValue(){ return perilVal; }
+	public float GetPerilValue(){ return perilValue; }
+	public float GetMaxPerilValue(){ return MAX_PERIL; }
 
 	/// Increase peril value
-	private void incrPerilValue(){
-		changePerilValue(1f, perilIncrRate);
+	private void IncrPerilValue(){
+//		ChangePerilValue(1f, perilIncrRate);
+		perilChangeRate = roomBreakSpeed;
+		perilChangeAmount = roomBreakAmount;
 	}
 
 	/// Decrease peril value
-	private void decrPerilValue(){
-		changePerilValue(-1f, perilDecrRate);
-		if(perilVal <= 0){ inPeril = false; }
+	private void DecrPerilValue(){
+//		ChangePerilValue(-1f, perilDecrRate);
+		perilChangeRate = roomHealSpeed;
+		perilChangeAmount = roomHealAmount;
+		if(perilValue <= MIN_PERIL){ inPeril = false; }
+	}
+
+	private void ManuallyDecrPerilValue(){
+		perilValue += roomHealAmount;
+		if(perilValue > MAX_PERIL){ perilValue = MAX_PERIL; }
+		if(perilValue < MIN_PERIL){ perilValue = MIN_PERIL; }
+		if(perilValue <= MIN_PERIL){ inPeril = false; }
+	}
+
+	private void DontChangePerilValue(){
+		perilChangeRate = 1f;
+		perilChangeAmount = 0;
 	}
 
 	/// Change peril value based dir (positive or negative)
 	/// and rate.
-	private void changePerilValue(float dir, float rate){
-		perilVal += dir * rate;
-		if(perilVal > MAX_PERIL){ perilVal = MAX_PERIL; }
-		if(perilVal < MIN_PERIL){ perilVal = MIN_PERIL; }
+	private IEnumerator ChangePerilValue(){
+		while(true){
+			yield return new WaitForSeconds(perilChangeRate);
+
+			if(inPeril){
+				perilValue += perilChangeAmount;
+				if(perilValue > MAX_PERIL){ perilValue = MAX_PERIL; }
+				if(perilValue < MIN_PERIL){ perilValue = MIN_PERIL; }
+
+			} else{ 
+				this.DontChangePerilValue();
+			}
+		}
 	}
 
 	/// Decide when a room is in peril
 	private IEnumerator ManagePerilTrigger(){
-
-
 		while(true) {
 			yield return new WaitForSeconds (1f);
-			// Only trigger when player is the room and room
-			// is not in peril
+
+			// Only trigger when player is not in the room
+			// and the room is not in peril
 			if(!plyrInRoom && !inPeril) {
+
+				// Count down (to 0)
 				perilCountDownTimer -= 1;
+
+				// Initiate increase in danger/chaos/peril
 				if(perilCountDownTimer <= 0) {
 					Debug.Log("!!!!!TRIGGERED!!!!!");
-					this.ResetPerilTrigger ();
+					this.ResetPerilTrigger();
 					inPeril = true;
 				}
 				Debug.Log("Peril Count Down Timer: " + perilCountDownTimer);
 			} else{ ResetPerilTrigger(); }
-
 		}
-
-
-
 	}
 
 	private void EscalalatePeril(){
