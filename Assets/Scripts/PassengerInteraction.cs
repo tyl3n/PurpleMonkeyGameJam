fcdec147e583c17c5aed746f2bb6d3ad09039797
Antoine McNabb-Baltar <antoine.mc-nabb-baltar.1@ens.etsmtl.ca>
2017-04-23 11:09:13 -0400

    Begining level

---
+++ Assets/Scripts/PassengerInteraction.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PassengerInteraction : MonoBehaviour {
+    bool CanInteract = false;
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	    if(CanInteract && Input.GetButton("Interaction"))
+        {
+            GameManager.instance.HappinessValue += 2;
+        }
+	}
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            CanInteract = true;
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            CanInteract = false;
+        }
+    }
+}
